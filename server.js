d7cc1d086cf4fbd8f9c9b5bf080f39608cafc01d
Glitch (bt-music-bot) <none>
2020-08-17 05:37:02 +0000

    🏹🧀 Checkpoint ./server.js:19753995/12

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ You can type ${prefix}help to get bot commands list`)
   
   try {
     const file = client.commands.get(cmd) || client.aliases.get(cmd)
-    if(!file) return msg.reply("exist")
+    if(!file) return msg.reply("nothing")
     
     const now = Date.now()
    if (db.has(`cooldown_${msg.author.id}`)) {
